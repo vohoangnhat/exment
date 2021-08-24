ede814254a53d4c2413dcd38b73b9f2be154b9c8
yisaka117 <yisaka@exceedone.co.jp>
2021-08-24 11:09:23 +0900

    add use Throwable

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace Exceedone\Exment\Exceptions;
 
 use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Session\TokenMismatchException;
 
@@ -30,7 +31,7 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return void
      */
     public function report(Throwable $exception)
@@ -42,7 +43,7 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return \Illuminate\Http\Response
      */
     public function render($request, Throwable $exception)
